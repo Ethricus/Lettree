e1815865407f4736cbd16984f993b38cbb42342f
Erithacus <92083124+MultidimensionalSock@users.noreply.github.com>
2024-11-01 08:19:02 +0000

    Update TreeNode.cs

--- Runtime/TreeNode.cs
+++ Runtime/TreeNode.cs
@@ -1,1 +1,23 @@
+namespace Lettree.Runtime
+{ 
+    public enum NodeState
+    {  
+        INACTIVE,
+        RUNNING, 
+        SUCCESS,
+        FAILURE
+    }
 
+    class TreeNode
+    {
+         protected TreeNode parent;
+         protected TreeNode[] children; 
+         public NodeState state { public get; private set; }
+
+         // add child 
+         // remove child 
+         // abort 
+         // Tick function 
+    }
+
+} 
