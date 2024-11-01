61b5e517f6f871abee68b5bb3aece672fcfb7e2c
Erithacus <92083124+MultidimensionalSock@users.noreply.github.com>
2024-11-01 11:48:23 +0000

    Update TreeNode.cs

--- Runtime/TreeNode.cs
+++ Runtime/TreeNode.cs
@@ -1,36 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Lettree.Runtime
-{ 
+{
     public enum NodeState
-    {  
+    {
         INACTIVE,
-        RUNNING, 
+        RUNNING,
         SUCCESS,
         FAILURE
     }
 
     class TreeNode
     {
-         protected TreeNode parent;
-         public TreeNode[] children { public get; private set; 
-         public NodeState state { public get; private set; }
-         
-         void Start()
-         {
-              children = new();
-         }
+        protected TreeNode parent;
+        public TreeNode[] children { get; private set; }
+        public NodeState state { get; private set; }
+        public bool HasChildren { get { return children.Length > 0; } }
+
+        void Start()
+        {
+            //children = new();
+        }
 
         public TreeNode GetChildNode(int index)
         {
             return children[index];
         }
 
-        public List<T> GetChildNodes<T>()
+        public List<T> GetChildNodes<T>() where T : TreeNode
         {
+            return null;
         }
 
-        public bool HasChildOfType<T>()
-
-        public bool HasChildren()
+        public bool HasChildOfType<T>() where T : TreeNode
+        {
+            foreach (TreeNode node in children)
+            {
+                if (node is T)
+                    return true;
+            }
+            return false;
+        }
     }
-
-} 
+}
